0e59f37e4ea4464d3bf2e7901489994092f9c355
Joel Almqvist <joeal360@student.liu.se>
2020-04-20 16:21:31 +0200

    Make print prettier

--- lab1a/print.py
+++ lab1a/print.py
@@ -7,7 +7,9 @@ plt.plot([1, 2,4,8,16, 32], [3.330033, 4.024569,3.482626, 3.254335, 3.582518, 4.
 plt.ylabel("Seconds")
 plt.xlabel("Number of cores")
 
-patch = mpatches.Patch(color='blue', label="Filter's execution time in seconds")
+plt.title("Threshfilter's execution time on image4 in seconds")
+
+patch = mpatches.Patch(color='blue', label="Execution time in seconds")
 plt.legend(handles=[patch])
 
 plt.savefig("thresh_filter")
