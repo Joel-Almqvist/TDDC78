7220fed684a3497c14ad49c61d7d4cedfc40a9f9
Joel Almqvist <joeal360@student.liu.se>
2020-04-20 18:10:47 +0200

    Add graph for blur

--- lab1a/print.py
+++ lab1a/print.py
@@ -2,16 +2,27 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 
-plt.plot([1, 2,4,8,16, 32], [3.330033, 4.024569,3.482626, 3.254335, 3.582518, 4.653898])
+plt.plot([1, 2,4,8,16, 32], [3.330033, 4.024569,3.482626, 3.254335, \
+    3.582518, 4.653898], color="blue")
 
 plt.ylabel("Seconds")
 plt.xlabel("Number of cores")
 
-plt.title("Threshfilter's execution time on image4 in seconds")
+plt.title("Filter execution time on 3000x3000 image with radius 10")
 
-patch = mpatches.Patch(color='blue', label="Execution time in seconds")
-plt.legend(handles=[patch])
+patch_thresh = mpatches.Patch(color='blue', label="Threshold")
+first_legend = plt.legend(handles=[patch_thresh], loc=(0.5,0.908))
 
-plt.savefig("thresh_filter")
+
+plt.gca().add_artist(first_legend)
+
+# Radius was set to 10 for these values
+plt.plot([1, 2,4,8, 16, 24, 28, 32], [4.736395, 4.034436, 4.040518, 4.099088, \
+    3.941547, 3.575454, 3.688609, 4.548544], color="red")
+
+patch_blur = mpatches.Patch(color='red', label="Blur")
+plt.legend(handles=[patch_blur], loc="upper right")
+
+plt.savefig("exectime_mpi")
 
 #plt.show()
